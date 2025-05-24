01a07476e16fa00f28cc339e63ad49bf3d78c606
crisbarch <cbartra@gmail.com>
2025-05-24 13:15:16 -0500

    cristian commit

---
+++ lib/cristian.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CristianScreen extends StatelessWidget {
+  const CristianScreen ({super.key});
+  @override
+  Widget @override
+  Widget build(BuildContext context) {
+    return ;
+  }
+}
